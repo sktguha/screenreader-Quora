f3aaedca3db55bfd1ba4f46d2dbaed8d7f8c59cf
sktguha <sktguha@gmail.com>
2014-12-19 14:05:50 +0530

    fixed indentation

--- navigation.js
+++ navigation.js
@@ -1,154 +1,142 @@
-mnl=5; mxl=200;
-function mfss(modern){
-    var sel = window.getSelection(); 
-    sel.modify("move","forward","character");
-    if(modern) //new version
-    sel.modify("move","backward","character");
-    
-    sel.modify("extend","forward","sentence");
-    
-    sel = window.getSelection(); 
-    if(sel.toString().length<mnl)
-        mfss();
+mnl = 5;
+mxl = 200;
+function mfss(modern) {
+	var sel = window.getSelection();
+	sel.modify("move", "forward", "character");
+	if (modern) // new version
+		sel.modify("move", "backward", "character");
+	sel.modify("extend", "forward", "sentence");
+	sel = window.getSelection();
+	if (sel.toString().length < mnl)
+		mfss();
 }
 /**
  * move backward by using system functions by one sentence
  */
-function mbss()
-{   mxl=200; mnl=10;
-    var sel = window.getSelection(); 
-    sel.modify("move","backward","character");
-    sel.modify("extend","backward","sentence");
+function mbss() {
+	mxl = 200;
+	mnl = 10;
+	var sel = window.getSelection();
+	sel.modify("move", "backward", "character");
+	sel.modify("extend", "backward", "sentence");
 
-    sel=window.getSelection();
-    if(sel.toString().length<mnl)
-        mbss();    
+	sel = window.getSelection();
+	if (sel.toString().length < mnl)
+		mbss();
 }
 
 /**
- *delim-char,min-length,max-length
- *move backward upto character array specified
+ * delim-char,min-length,max-length move backward upto character array specified
  */
-function mbc()
-{
-	ppa=[';',':','.','(',')',',','\n','?'];
-    min=4; max=300;
-    var selection = window.getSelection(); 
-    selection.modify("move","backward","character");
-    var ct=0;
-    while(true)
-    {
-        ct++;
-        if(ct>=inf)
-        { 
-                
-            break; /*end of document */
-        }
-         
-        ecb();
-        var txt=window.getSelection().toString();
-        var fch=txt.charAt(0);
-        if(txt.length>max)
-            break;
-	    //console.log(cts(ppa,txt));
-        if(ppa.indexOf(fch)!=-1 && txt.length>min)
-        {
-               
-            break;
-        }
-    }
-}
-/** 
- *delim-char,min-length,max-length
- *move forward upto character array specified
- */
-var inf=1000;
-function mfc()
-{
-	npa=[';',':','.','(',')',',','\n','?'];
-	min=3; max=300;
-	//console.log("npa is "+npa);
-    var selection = window.getSelection(); 
-    selection.modify("move","forward","character");
-    var ct=0;
-    while(true)
-    {
-        ct++;
-        if(ct>=inf)
-        { 
-                
-            break; /*end of document */
-        }
-         
-        ecf();
-        var txt=window.getSelection().toString();
-        var fch=txt.charAt(txt.length-1);
-        if(txt.length>max)
-            break;
-        //console.log(cts(npa,txt));
-        if(npa.indexOf(fch)!=-1 && txt.length >min)
-        { 
+function mbc() {
+	ppa = [ ';', ':', '.', '(', ')', ',', '\n', '?' ];
+	min = 4;
+	max = 300;
+	var selection = window.getSelection();
+	selection.modify("move", "backward", "character");
+	var ct = 0;
+	while (true) {
+		ct++;
+		if (ct >= inf) {
 
-            break;
-        }
-    }
-} 
+			break; /* end of document */
+		}
 
-function ecf()
-{
-   
-    var sel=window.getSelection();
-    sel.modify("extend","forward","character");
-}
-function ecb()
-{
-    var sel=window.getSelection();
-    sel.modify("extend","backward","character");
-}
+		ecb();
+		var txt = window.getSelection().toString();
+		var fch = txt.charAt(0);
+		if (txt.length > max)
+			break;
+		// console.log(cts(ppa,txt));
+		if (ppa.indexOf(fch) != -1 && txt.length > min) {
 
-function mfw()
-{
-    var sel=window.getSelection();   
-    sel.modify("move","forward","character");
-    sel.modify("extend","forward","word");  
+			break;
+		}
+	}
 }
 /**
- *move backword by word
- **/
-function mbw()
-{
-    var sel=window.getSelection();   
-    sel.modify("move","backward","character");
-    sel.modify("extend","backward","word");  
+ * delim-char,min-length,max-length move forward upto character array specified
+ */
+var inf = 1000;
+function mfc() {
+	npa = [ ';', ':', '.', '(', ')', ',', '\n', '?' ];
+	min = 3;
+	max = 300;
+	// console.log("npa is "+npa);
+	var selection = window.getSelection();
+	selection.modify("move", "forward", "character");
+	var ct = 0;
+	while (true) {
+		ct++;
+		if (ct >= inf) {
+
+			break; /* end of document */
+		}
+
+		ecf();
+		var txt = window.getSelection().toString();
+		var fch = txt.charAt(txt.length - 1);
+		if (txt.length > max)
+			break;
+		// console.log(cts(npa,txt));
+		if (npa.indexOf(fch) != -1 && txt.length > min) {
+
+			break;
+		}
+	}
 }
 
-function mvis()  
-{
-    var scf=0.3;
-    var ele=window.getSelection().getRangeAt(0);        
-    var sel = ele.getBoundingClientRect();               
-    var dw  = document.documentElement.clientWidth;     
-    var dh = document.documentElement.clientHeight;       
-    dw=window.innerWidth;
-    dh=window.innerHeight;
-    var st=sel.top;
-    var sb=sel.bottom;
-    var db=dh; 
-    var dt=0;
-    var amt=0;
-    if(st<0)     //too much up
-    {
-        //alert("st<0");
-        amt=-1*dh*scf;//341 dh*scf; 
-    }
-    if(sb>db) //too much down
-    {
-        //alert("sb>db");
-        amt=dh*scf; //341;    
-    }
-    window.scrollBy(0, amt);
-     //console.log(sel.top+" " +sel.right+" "+sel.bottom+" "+ sel.left+"\n"+dw+" "+dh+" amt= "+amt);        
-	 //console.error("scroll value "+amt);	 
-	if(amt>500) 
-        console.debug("scroll value too much "+amt);
+function ecf() {
+
+	var sel = window.getSelection();
+	sel.modify("extend", "forward", "character");
+}
+function ecb() {
+	var sel = window.getSelection();
+	sel.modify("extend", "backward", "character");
+}
+
+function mfw() {
+	var sel = window.getSelection();
+	sel.modify("move", "forward", "character");
+	sel.modify("extend", "forward", "word");
+}
+/**
+ * move backword by word
+ */
+function mbw() {
+	var sel = window.getSelection();
+	sel.modify("move", "backward", "character");
+	sel.modify("extend", "backward", "word");
+}
+
+function mvis() {
+	var scf = 0.3;
+	var ele = window.getSelection().getRangeAt(0);
+	var sel = ele.getBoundingClientRect();
+	var dw = document.documentElement.clientWidth;
+	var dh = document.documentElement.clientHeight;
+	dw = window.innerWidth;
+	dh = window.innerHeight;
+	var st = sel.top;
+	var sb = sel.bottom;
+	var db = dh;
+	var dt = 0;
+	var amt = 0;
+	if (st < 0) // too much up
+	{
+		// alert("st<0");
+		amt = -1 * dh * scf;// 341 dh*scf;
+	}
+	if (sb > db) // too much down
+	{
+		// alert("sb>db");
+		amt = dh * scf; // 341;
+	}
+	window.scrollBy(0, amt);
+	// console.log(sel.top+" " +sel.right+" "+sel.bottom+" "+ sel.left+"\n"+dw+"
+	// "+dh+" amt= "+amt);
+	// console.error("scroll value "+amt);
+	if (amt > 500)
+		console.debug("scroll value too much " + amt);
 }
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 (function fakers(){
-//document.getElementsByClassName('ErrorBanner fade_in')[0].style.display='none';
-//[].forEach.call(document.getElementsByClassName('vote_item_link add_upvote'),function(u){ u.onclick=function(){ return false;}});
-//[].forEach.call(document.getElementsByTagName("*"),function(el){ el.onclick=function(e){ console.log(e.srcElement); return false;} });
-//location={href:'www.quora.com'}
-f=1;
+//	document.getElementsByClassName('ErrorBanner fade_in')[0].style.display='none';
+//	[].forEach.call(document.getElementsByClassName('vote_item_link add_upvote'),function(u){ u.onclick=function(){ return false;}});
+//	[].forEach.call(document.getElementsByTagName("*"),function(el){ el.onclick=function(e){ console.log(e.srcElement); return false;} });
+//	location={href:'www.quora.com'}
+	f=1;
 })();
 speak(location.href);
 var index=0;
@@ -16,108 +16,108 @@ var cpl=pl[index];
 var tagcounter=0;
 var quecounter=0;
 document.onkeydown=function(e){ 
-var kc=(e.keyCode) ? e.keyCode : e.which;
-var ks=String.fromCharCode(kc).toLowerCase();
-if(ks=='t') tagcounter++;
-else tagcounter=0;
+	var kc=(e.keyCode) ? e.keyCode : e.which;
+	var ks=String.fromCharCode(kc).toLowerCase();
+	if(ks=='t') tagcounter++;
+	else tagcounter=0;
 
-if(ks=='q') quecounter++;
-else quecounter=0;
+	if(ks=='q') quecounter++;
+	else quecounter=0;
 
-if(tagcounter>=9) {
-	where() && window.open(where().getElementsByClassName('topic_name')[0].href );
-	tagcounter=0;
-}
+	if(tagcounter>=9) {
+		where() && window.open(where().getElementsByClassName('topic_name')[0].href );
+		tagcounter=0;
+	}
 
-if(quecounter>=9) {
-	where() && window.open(where().getElementsByClassName('question_link')[0].href );
-	quecounter=0;
-}
+	if(quecounter>=9) {
+		where() && window.open(where().getElementsByClassName('question_link')[0].href );
+		quecounter=0;
+	}
 
 }
 
 window.onblur=function(){ window.speechSynthesis.cancel(); }
 document.onkeypress=function(e){
-    var kc=(e.keyCode) ? e.keyCode : e.which;
-    var ks=String.fromCharCode(kc).toLowerCase();
+	var kc=(e.keyCode) ? e.keyCode : e.which;
+	var ks=String.fromCharCode(kc).toLowerCase();
 	console.log("kc"+kc+" string "+ks);
-    //mvis();
-    if(kc==13){  //enter key
+	//mvis();
+	if(kc==13){  //enter key
 		var curr;
 		if(window.getSelection().getRangeAt(0)) {
 			curr=window.getSelection().focusNode;
 			curr=curr.parentElement;
 		}
-    	curr && curr.click && curr.click() && console.log("click from enter");
-    }
-    if(ks==","){
-        mfss();
-        triggerLoad(where() || cpl);
-        speak(window.getSelection().toString());
-    }
-    if(ks=="m"){
-        mbss();
-        triggerLoad(where() || cpl);
-        speak(window.getSelection().toString());
-    } 
-    if(ks=="'"){
-      mfc();
-      triggerLoad(where() || cpl);
-      speak(window.getSelection().toString());  
-    }
-    if(ks==";"){
-      mbc();
-      triggerLoad(where() || cpl);
-      speak(window.getSelection().toString());  
-    }
-    if(ks=="]"){
-      mfw();
-      triggerLoad(where() || cpl);
-      speak(window.getSelection().toString());  
-    }
-    if(ks=="["){
-      mbw();
-      triggerLoad(where() || cpl);
-      speak(window.getSelection().toString());  
-    }
-    if(ks=="w") //where am I
-      speak(location.href);
-    if(ks=="/"){
-      shift(1);
-      triggerLoad(where() || cpl);
-      speak(window.getSelection().toString());
-    }
+		curr && curr.click && curr.click();
+	}
+	if(ks==","){
+		mfss();
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());
+	}
+	if(ks=="m"){
+		mbss();
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());
+	} 
+	if(ks=="'"){
+		mfc();
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());  
+	}
+	if(ks==";"){
+		mbc();
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());  
+	}
+	if(ks=="]"){
+		mfw();
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());  
+	}
+	if(ks=="["){
+		mbw();
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());  
+	}
+	if(ks=="w") //where am I
+		speak(location.href);
+	if(ks=="/"){
+		shift(1);
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());
+	}
 	if(ks=="."){
-	  shift(0);
-	  triggerLoad(where() || cpl);
-      speak(window.getSelection().toString());
+		shift(0);
+		triggerLoad(where() || cpl);
+		speak(window.getSelection().toString());
 	}
 	if("m,.;'\[\]".indexOf(ks)!=-1){
 		mvis();
 	}
 	if(ks=="t")
-	  speakTags();
+		speakTags();
 	if(ks=='u')
-	  upvote();
+		upvote();
 	if(ks=='d')
-	  downvote();
+		downvote();
 	if(ks=='c')
-	  commentSpeak();
+		commentSpeak();
 	if(ks=='s')
-	  share(); 
+		share(); 
 	if(ks=='n'){
 		window.open("http://www.quora.com/notifications");
-	   }
-	 if(ks=='q'){
-	 	speakQues();
-	 } 
+	}
+	if(ks=='q'){
+		speakQues();
+	} 
 }
 //Array.prototype.some.call(pl,function(e,i,arr){  cpl=pl[i]; return e.getElementsByClassName("QuestionText")[0];})
 //pl=Array.prototype.filter.call(pl,function(p){ return p.getElementsByClassName("QuestionText")[0] }); 
 
 function commentSpeak(){
 	var dv=where() || cpl;
-    var commentLink=dv.getElementsByClassName('view_comments supp ')[0];
+	var commentLink=dv.getElementsByClassName('view_comments supp ')[0];
 	commentLink && commentLink.click();
 	var lc=function(){
 		var comments=dv.getElementsByClassName('threaded_comments')[0];
@@ -136,12 +136,12 @@ function downvote(){
 	if(!dv) return;
 	var downvoteBtn=dv.getElementsByClassName('downvote')[0];
 	if(downvoteBtn.innerText.trim().toLowerCase()=='downvote'){
-	    downvoteBtn.click();
-	    speak('downvoted');
+		downvoteBtn.click();
+		speak('downvoted');
 	}
 	else if(downvoteBtn){
-	    downvoteBtn.click();
-	    speak('downvote removed');
+		downvoteBtn.click();
+		speak('downvote removed');
 	}
 }
 
@@ -150,15 +150,15 @@ function upvote(){
 	if(!div) return;
 	var upvoteBtn=div.getElementsByClassName('vote_item_link add_upvote')[0];
 	if(upvoteBtn){
-     upvoteBtn.click();
-     speak('upvoted');
+		upvoteBtn.click();
+		speak('upvoted');
 	}
 	else if( upvoteBtn = div.getElementsByClassName('vote_item_link remove_upvote')[0]){
-	upvoteBtn.click();
-	speak('upvote removed');	
+		upvoteBtn.click();
+		speak('upvote removed');	
 	}
-    
-	}
+
+}
 
 function share(){
 	var dv=where() || cpl;
@@ -169,7 +169,7 @@ function share(){
 	var timeId= setInterval(function(){
 		counter++;  
 		var shareBtn=[].filter.call(document.getElementsByTagName("input"),function(e){ 
-		return e.className=='submit_button' && e.value=="Share"})[0];
+			return e.className=='submit_button' && e.value=="Share"})[0];
 		if(shareBtn){ 
 			shareBtn.click();
 			clearInterval(timeId); 
@@ -179,88 +179,88 @@ function share(){
 			clearInterval(timeId); 
 			console.error("network error "+timeId);
 		}
-		},800);
+	},800);
 }
 
 function speakTags(){
 	var div=where() || cpl;
 	if(!div) return;
 	var tag=div.getElementsByClassName('TopicName')[0].innerText;
-    speak(tag);
+	speak(tag);
 }
 
 function speakQues(){
 	var div=where() || cpl;
 	if(!div) return;
 	var quesText=div.getElementsByClassName('QuestionText')[0].innerText;
-    speak(quesText);
+	speak(quesText);
 }
 
 function updateIndex(div){
-	
+
 	if(div){
-	for(var i=0;pl[i];i++){
-	 if(pl[i]==div){
-	 index=i;
-	 console.log('updated index is ',index);
-	 break;	
-	 }	
+		for(var i=0;pl[i];i++){
+			if(pl[i]==div){
+				index=i;
+				console.log('updated index is ',index);
+				break;	
+			}	
+		}
 	}
-}
 }
 
 function shift(d){
-where() && updateIndex(where());
-var dv=get(d);
-console.log('got from get',dv);
-if(!dv) return;
-triggerLoad(dv);
-dv.getElementsByClassName("QuestionText")[0] &&	(dv=dv.getElementsByClassName("QuestionText")[0]);
-placeAtStart(dv); 
-mfss(true);
-//where() && where().scrollIntoView();
-//mvis();
-cpl.scrollIntoView();
+	where() && updateIndex(where());
+	var dv=get(d);
+	console.log('got from get',dv);
+	if(!dv) return;
+	triggerLoad(dv);
+	dv.getElementsByClassName("QuestionText")[0] &&	(dv=dv.getElementsByClassName("QuestionText")[0]);
+	placeAtStart(dv); 
+	mfss(true);
+//	where() && where().scrollIntoView();
+//	mvis();
+	cpl.scrollIntoView();
 }
 
 function isvisible(dv){
-return dv && (dv.getClientRects()[0] && dv.getClientRects()[0].height) ;
+	return dv && (dv.getClientRects()[0] && dv.getClientRects()[0].height) ;
 }
 
 //start loading contents after 1.5 sec and focus not moved from current pagedlist_item
 function triggerLoad(div){
- 	//console.log('passing arg',dv);
+	//console.log('passing arg',dv);
 	window.setTimeout(
-	function(){
-	//console.log('load triggered',dv.id,where().id);
-	if(where().id==div.id){
-	console.debug('click');
-	var expand=[].filter.call(div.getElementsByClassName('more_link'),function(a){ return a.getAttribute('action_click')=="ContentExpand";})[0];
-	expand && expand.click();  
-	}
-	} ,1500);
+			function(){
+				//console.log('load triggered',dv.id,where().id);
+				if(where().id==div.id){
+					console.debug('click');
+					var expand=[].filter.call(div.getElementsByClassName('more_link'),function(a){ return a.getAttribute('action_click')=="ContentExpand";})[0];
+					expand && expand.click();  
+				}
+			} ,1500);
 };
 
 function get(d){
 	//down
 	if(d){
-	for(var i=1;pl[index+i];i++){
-	if(pl[index+i] && isvisible(pl[index+i])){
-	//if(true)// isvisible(pl[index+1])){
-		index+=i;
-		cpl=pl[index];
-		return cpl;
-	}
-	}
-	}else{
-	for(var i=1;pl[index-i];i--){
-	if(pl[index-i] && isvisible(pl[index-i]) ){
-		//if( true){ //isvisible(pl[index-1])){
-			index-=i;
-			cpl=pl[index];
-			return cpl;
+		for(var i=1;pl[index+i];i++){
+			if(pl[index+i] && isvisible(pl[index+i])){
+				//if(true)// isvisible(pl[index+1])){
+				index+=i;
+				cpl=pl[index];
+				return cpl;
+			}
 		}
-	}
+	}else{
+		for(var i=1;pl[index-i];i--){
+			if(pl[index-i] && isvisible(pl[index-i]) ){
+				//if( true){ //isvisible(pl[index-1])){
+				index-=i;
+				cpl=pl[index];
+				return cpl;
+			}
+		}
 	}
 	return cpl;
 }
@@ -291,43 +291,43 @@ function speakinit()
 }
 function speak(text)
 {
-var utterance = new SpeechSynthesisUtterance(text);
-//var rate=parseInt(text.split("<speedofvoice1389867680568>")[1]);
-var voices = window.speechSynthesis.getVoices();
-var ofv=voices.filter(function(v){return v.localService==true})[0];
+	var utterance = new SpeechSynthesisUtterance(text);
+//	var rate=parseInt(text.split("<speedofvoice1389867680568>")[1]);
+	var voices = window.speechSynthesis.getVoices();
+	var ofv=voices.filter(function(v){return v.localService==true})[0];
 
-if(ofv){
-utterance.voice=ofv;
-}
-window.speechSynthesis.cancel();
-window.speechSynthesis.speak(utterance);
+	if(ofv){
+		utterance.voice=ofv;
+	}
+	window.speechSynthesis.cancel();
+	window.speechSynthesis.speak(utterance);
 
 }
 
 function where(){
-var el;
-try{ 
-el=window.getSelection().getRangeAt(0);
-}catch(e){
-	return;
-}
-if(!el) return;
-el=el.startContainer;
-while(el && el!=document.body){
-el=el.parentElement;
-if(el.className=="pagedlist_item")
-break;
-}
-if(el && el.className=="pagedlist_item")
-return el;
+	var el;
+	try{ 
+		el=window.getSelection().getRangeAt(0);
+	}catch(e){
+		return;
+	}
+	if(!el) return;
+	el=el.startContainer;
+	while(el && el!=document.body){
+		el=el.parentElement;
+		if(el.className=="pagedlist_item")
+			break;
+	}
+	if(el && el.className=="pagedlist_item")
+		return el;
 
-el=el.endContainer;
-while(el && el!=document.body){
-el=el.parentElement;
-if(el.className=="pagedlist_item")
-break;
-}
-if(el && el.className=="pagedlist_item")
-return el;
+	el=el.endContainer;
+	while(el && el!=document.body){
+		el=el.parentElement;
+		if(el.className=="pagedlist_item")
+			break;
+	}
+	if(el && el.className=="pagedlist_item")
+		return el;
 
 }
