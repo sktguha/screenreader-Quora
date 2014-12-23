dedb6a1883c8e8b46ca11aa7b4269fcf4bc69f30
Saikat Guha <sktguha@gmail.com>
2014-12-23 13:53:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 //	location={href:'www.quora.com'}
 	f=1;
 })();
-speak(location.href);
+speak(document.title || location.href);
 var index=0;
 var root=[].reduce.call(document.getElementsByClassName("PagedList"),function(p,n){ return p.children.length>n.children.length?p:n; })
 //document.getElementsByClassName("Feed PagedList UniversalPageFeed");
